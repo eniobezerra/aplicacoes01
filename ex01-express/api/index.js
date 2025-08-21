a888b596e1c2cc867aa25d0a83ee7c86c6a6e45a
eniobezerra <142815890+eniobezerra@users.noreply.github.com>
2025-08-21 23:12:05 +0000

    1 commit

--- ex01-express/api/index.js
+++ ex01-express/api/index.js
@@ -8,7 +8,7 @@ console.log("PYTHON_ROOT", process.env.PYTHON_ROOT);
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Bem-vindo ao Express de Márcio");
+  res.send("Bem-vindo ao Express de enio");
 });
 
 app.listen(3000, () => {
